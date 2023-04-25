33624102a364b9d0cdd220a71e57af0b1260a03a
l3miage-koneze <zeinaboubissikone@gmail.com>
2023-04-25 15:04:35 +0100

    Reprise creatorComponent

--- src/app/creator/creator.component.ts
+++ src/app/creator/creator.component.ts
@@ -1,47 +1,38 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { OnInit } from '@angular/core';
-
-export interface Question {
-  question: string;
-  reponses: string[];
-  reponseCorrecte: string;
-}
 
 @Component({
   selector: 'app-creator',
   templateUrl: './creator.component.html',
-  styleUrls: ['./creator.component.scss']
+  styleUrls: ['./creator.component.css']
 })
+export class CreatorComponent {
+  question !: string;
+  answers ! : string[];
+  correctAnswer !: number;
 
-
-export class CreatorComponent implements OnInit {
-
-  questions ! : Question[];   //l'ensemble des questions-réponses du Miahoot
 
   constructor(private http: HttpClient) {}
-  
-  ngOnInit() {
-    this.getQuestions().subscribe(questions => this.questions = questions);
-  }
-  
-  getQuestions(): Observable<Question[]> {
-    return this.http.get<Question[]>('https://mon-backend.com/miahoot/questions');  //on récupère questions via les requêtes http depuis le backend
+
+  submitMiahoot(): void {
+    const data = {
+      question: this.question,
+      answers: this.answers,
+      correctAnswer: this.correctAnswer
+    };
+    this.createMiahoot(data).subscribe(
+      () => {
+        console.log('Miahoot created successfully!');
+      },
+      (error) => {
+        console.log(`Failed to create Miahoot: ${error}`);
+      }
+    );
   }
 
-  
-  submitMiahoot() {
-    const reponsesParticipant: { question: string; reponse: string[]; }[] = [];
-    
-    this.questions.forEach(q => {
-      const reponseChoisie = document.querySelector(`input[name="${q.question}"]:checked`);
-      //reponsesParticipant.push({ question: q.question, reponse: reponseChoisie? || 'N/A' });
-    });
-    
-    this.http.post('https://mon-backend.com/miahoot/create', reponsesParticipant).subscribe(() => {
-      // Ici, on peut peut-être afficher un message de confirmation de l'utilisateur
-    });
+  private createMiahoot(data: any): Observable<any> {
+    const url = 'url du backend';
+    return this.http.post(url, data);
   }
-  
 }
