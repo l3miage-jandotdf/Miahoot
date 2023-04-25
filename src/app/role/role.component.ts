7f3a6c3d45fa8cd5a3e6309399cfa928ccf9e40d
l3miage-wyssm <mcnw2001@gmail.com>
2023-04-25 11:52:39 +0200

    Ajout du bouton pour passer de concepteur a presentateur et vice versa

--- src/app/role/role.component.ts
+++ src/app/role/role.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+enum Role{
+  Concepteur = "concepteur",
+  Presentateur = "presentateur"
+}
 
 @Component({
   selector: 'app-role',
   templateUrl: './role.component.html',
   styleUrls: ['./role.component.scss']
 })
-export class RoleComponent {
+
+
+export class RoleComponent implements OnInit{
+  role : Role = Role.Concepteur;
+  concepteur : boolean = true;
+
+
+  updateRole() : void{
+    if(this.role==Role.Concepteur){
+      this.role=Role.Presentateur;
+      this.concepteur=false;
+    }
+    else{
+      this.role=Role.Concepteur;
+      this.concepteur=true;
+    }
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
 
 }
