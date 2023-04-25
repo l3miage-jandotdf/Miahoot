cdd035c50225229d218426f8e06adc726a7e243d
l3miage-koneze <zeinaboubissikone@gmail.com>
2023-04-25 14:17:18 +0100

    CreatorComponent : version préliminaire non complète

--- src/app/creator/creator.component.ts
+++ src/app/creator/creator.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { OnInit } from '@angular/core';
+
+export interface Question {
+  question: string;
+  reponses: string[];
+  reponseCorrecte: string;
+}
 
 @Component({
   selector: 'app-creator',
   templateUrl: './creator.component.html',
   styleUrls: ['./creator.component.scss']
 })
-export class CreatorComponent {
 
+
+export class CreatorComponent implements OnInit {
+
+  questions ! : Question[];   //l'ensemble des questions-réponses du Miahoot
+
+  constructor(private http: HttpClient) {}
+  
+  ngOnInit() {
+    this.getQuestions().subscribe(questions => this.questions = questions);
+  }
+  
+  getQuestions(): Observable<Question[]> {
+    return this.http.get<Question[]>('https://mon-backend.com/miahoot/questions');  //on récupère questions via les requêtes http depuis le backend
+  }
+
+  
+  submitMiahoot() {
+    const reponsesParticipant: { question: string; reponse: string[]; }[] = [];
+    
+    this.questions.forEach(q => {
+      const reponseChoisie = document.querySelector(`input[name="${q.question}"]:checked`);
+      //reponsesParticipant.push({ question: q.question, reponse: reponseChoisie? || 'N/A' });
+    });
+    
+    this.http.post('https://mon-backend.com/miahoot/create', reponsesParticipant).subscribe(() => {
+      // Ici, on peut peut-être afficher un message de confirmation de l'utilisateur
+    });
+  }
+  
 }
