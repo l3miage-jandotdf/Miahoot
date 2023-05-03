25aa66d40628dd21eecefb2db2b465c11271bc44
l3miage-xusi <121896780+l3miage-xusi@users.noreply.github.com>
2023-05-03 16:10:11 +0200

    Lianson Creator/Google

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { DocumentSnapshot, Firestore, FirestoreDataConverter, QueryDocumentSnapshot, SnapshotOptions, doc, docData, getDoc } from '@angular/fire/firestore';
 import { Auth, authState } from '@angular/fire/auth';
@@ -6,9 +7,9 @@ import { setDoc } from 'firebase/firestore';
 import { MatIconAnchor } from '@angular/material/button';
 
 
-export interface MiahootUser{
-  readonly name : string | null,
-  readonly photoUrl : string | null
+export interface MiahootUser {
+  readonly name: string | null,
+  readonly photoUrl: string | null
 }
 
 @Injectable({
@@ -17,7 +18,7 @@ export interface MiahootUser{
 
 export class DataService {
 
-  constructor(private auth : Auth, private fireStore : Firestore) { 
+  constructor(private auth: Auth, private fireStore: Firestore, private http: HttpClient) {
   }
 
   getMiahootUser$(): Observable<MiahootUser | undefined> {
@@ -28,16 +29,16 @@ export class DataService {
 
           let docQuelconque = doc(this.fireStore, `users/${user.uid}`);
           let refDocQuelconque = docQuelconque.withConverter(MiahootUserConverter);
-          let obsDoc : Observable<MiahootUser> = docData(refDocQuelconque);
-          let promesSnap : Promise<DocumentSnapshot<MiahootUser>> = getDoc(refDocQuelconque);
+          let obsDoc: Observable<MiahootUser> = docData(refDocQuelconque);
+          let promesSnap: Promise<DocumentSnapshot<MiahootUser>> = getDoc(refDocQuelconque);
           promesSnap.then(snapshot => {
-            if(!snapshot.exists()) {
-              let utilisateurParDefaut : MiahootUser = {name : user.displayName, photoUrl : user.photoURL}; 
+            if (!snapshot.exists()) {
+              let utilisateurParDefaut: MiahootUser = { name: user.displayName, photoUrl: user.photoURL };
               setDoc(snapshot.ref, utilisateurParDefaut);
             }
           });
           return obsDoc;
-          
+
         } else {
           // L'utilisateur n'est pas connecté
           return of(undefined);
@@ -45,14 +46,21 @@ export class DataService {
       })
     );
   }
+  createCreator(creatorData: any): Observable<any> {
+    return this.http.post('http://localhost:8080/api/creator/', creatorData);
+  }
+
+  checkIfCreatorExists(uid: string): Observable<boolean> {
+    return this.http.get<boolean>(`/api/creator/check/${uid}`);
+  }
 }
 
-export const MiahootUserConverter =  {
-   toFirestore(user: MiahootUser): MiahootUser {
+export const MiahootUserConverter = {
+  toFirestore(user: MiahootUser): MiahootUser {
     return user;
   },
 
-   fromFirestore(snapshot: QueryDocumentSnapshot<MiahootUser>, options: SnapshotOptions): MiahootUser {
+  fromFirestore(snapshot: QueryDocumentSnapshot<MiahootUser>, options: SnapshotOptions): MiahootUser {
     const data = snapshot.data();
     const name = data?.name ?? '';
     const photoUrl = data?.photoUrl ?? '';
@@ -66,26 +74,29 @@ export const MiahootUserConverter =  {
    * Structures de données pour les Miahoot projetés
    */
 
-export interface MiahootProjected{
-    readonly id  : string;
-    creator : string;
-    presentator : string ;
-    currentMCQ : string;
-    //QCMs : QcmProjected;
+export interface MiahootProjected {
+  readonly id: string;
+  creator: string;
+  presentator: string;
+  currentMCQ: string;
+  //QCMs : QcmProjected;
 
 }
 
 export type VOTES = {
-  [participantUID:string] : true    
+  [participantUID: string]: true
 }
 
 
 export interface QcmProjected {
-  question : string;
-  responses : string[]; //réponses possibles
-  votes : VOTES[]; //Autant d'entrée dans le tableau que de réponses possibles
-
+  question: string;
+  responses: string[]; //réponses possibles
+  votes: VOTES[]; //Autant d'entrée dans le tableau que de réponses possibles
 }
+
+
+
+
 
 /*export const FsMiahootProjectedConverter : FirestoreDataConverter<MiahootProjected> = {
   toFirestore : M => M,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Optional, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Optional, Output } from '@angular/core';
 import { Auth, authState, signInAnonymously, signOut, User, GoogleAuthProvider, signInWithPopup } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -7,7 +7,6 @@ import { DataService, MiahootUser } from '../data.service';
 import { traceUntilFirst } from '@angular/fire/performance';
 import { setDoc } from 'firebase/firestore';
 import { HttpClient } from '@angular/common/http';
-import { Location } from '@angular/common';
 
 
 @Component({
@@ -17,11 +16,9 @@ import { Location } from '@angular/common';
 })
 
 export class LoginComponent implements OnInit {
-  @Output() log = new EventEmitter<boolean>();
-
 
   public readonly user$?: Observable<User | null>;
-  public currentUser? : User | null;
+  public currentUser?: User | null;
   miahootUser: MiahootUser | undefined;
   pageCreation = false;
 
@@ -30,25 +27,29 @@ export class LoginComponent implements OnInit {
 
   public isAuthenticating = false;
 
+  @Output() log: EventEmitter<boolean> = new EventEmitter<boolean>();
+
   constructor(@Optional() private auth: Auth, private router: Router, private dataService: DataService, private http: HttpClient) {
     if (auth) {
       this.user$ = authState(this.auth);
       this.isLoggedIn$ = authState(this.auth).pipe(
         traceUntilFirst('auth'),
         map(u => {
-        this.currentUser = u;
-        return !!u})
+          this.currentUser = u;
+          return !!u
+        })
       );
-      this.pageCreation=false;
+      this.pageCreation = false;
     }
   }
-/*
-  createNewMiahoot() {
-    this.http.post('http://localhost:8080/api/miahoot', {}).subscribe(() => {
-      console.log("MIAHOOT CREE !!")
-    });
-  }
-*/
+
+  /*
+    createNewMiahoot() {
+      this.http.post('http://localhost:8080/api/miahoot', {}).subscribe(() => {
+        console.log("MIAHOOT CREE !!")
+      });
+    }
+  */
   afficheMiahoot() {
     this.http.get('http://localhost:8080/api/miahoot/nom/testM', {}).subscribe((response) => {
       console.log(response);
@@ -56,7 +57,7 @@ export class LoginComponent implements OnInit {
     });
   }
   createNewMiahoot() {
-    this.http.post('http://localhost:8080/api/miahoot/', {"nom":"testM"}).subscribe((response) => {
+    this.http.post('http://localhost:8080/api/miahoot/', { "nom": "testM" }).subscribe((response) => {
       this.afficheMiahoot();
       console.log("MIAHOOT CREE !!");
     });
@@ -65,55 +66,85 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.dataService.getMiahootUser$().subscribe(user => {
       this.miahootUser = user;
-      console.log ("y'a de l'action");
+      console.log("y'a de l'action");
     });
-
-    console.log("url = ",this.router.url);
-    if(this.router.url=="/"){
-      this.log.emit(false);
-    }
-    if(this.router.url=="/*"){
-      this.log.emit(true);
-    }
-    }
-
-  async login() {
-    this.isAuthenticating = true; // On indique que l'authentification est en cours
-    try {
-      await signInWithPopup(this.auth, new GoogleAuthProvider());
-    }
-      finally {
-      this.isAuthenticating = false; // On réinitialise la variable une fois que la promesse est résolue
-    }
-    this.log.emit(true);
-    this.goToPage('/accueil')
   }
-
+  /*
+    async login() {
+      this.isAuthenticating = true; // On indique que l'authentification est en cours
+      try {
+        await signInWithPopup(this.auth, new GoogleAuthProvider());
+      }
+        finally {
+        this.isAuthenticating = false; // On réinitialise la variable une fois que la promesse est résolue
+      }
+    }
+  */
   async loginAnonymously() {
     return await signInAnonymously(this.auth);
   }
 
-  async logout() {
-    this.pageCreation=false;
-    this.goToPage('');
-    this.log.emit(false);
-    return await signOut(this.auth);
-
+  async login() {
+    this.isAuthenticating = true;
+    try {
+      const result = await signInWithPopup(this.auth, new GoogleAuthProvider());
+      const user = result.user;
+      if (user) {
+        this.currentUser = user;
+        const creatorData = {
+          uid: user.uid,
+          nom: user.displayName,
+          photo: user.photoURL,
+        };
+        this.dataService.checkIfCreatorExists(user.uid).subscribe((exists) => {
+          if (!exists) {
+            this.createCreator(creatorData);
+          } else {
+            console.log('User already exists');
+          }
+        });
+        this.log.emit(true);
+      }
+    } finally {
+      this.isAuthenticating = false;
+    }
+  }
+  createCreator(creatorData: any) {
+    this.dataService.createCreator(creatorData).subscribe(
+      (response) => {
+        console.log('Creator a creé', response);
+      },
+      (error) => {
+        console.log('Creator a echoué', error);
+      }
+    );
   }
 
-  goToPage(pageName:string){
-    /*if(pageName=="**"){
-      this.log.emit(true);
-    }*/
-    //console.log(pageName);
+  goToPage(pageName: string) {
+    console.log(pageName);
     this.router.navigate([`${pageName}`]);
   }
 
 
-  updatePage() : void{
-    this.pageCreation=true;
+  /* async logout() {
+     this.pageCreation=false;
+     return await signOut(this.auth);
+   }
+ 
+   goToPage(pageName:string){
+     if(pageName=="/creator"){
+       this.pageCreation=true;
+     }
+     console.log(pageName);
+     this.router.navigate([`${pageName}`]);
+   }*/
+
+  updatePage(): void {
+    this.pageCreation = true;
   }
 
+  async logout() {
+    return await signOut(this.auth);
+  }
 
 }
-
