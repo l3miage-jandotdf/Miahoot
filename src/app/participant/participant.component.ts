8e671347382697a3a9f5bdb881eeb7f00c98553b
l3miage-wyssm <mcnw2001@gmail.com>
2023-05-04 13:45:55 +0200

    reajout de participant

---
+++ src/app/participant/participant.component.ts
@@ -0,0 +1,56 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-participant',
+  templateUrl: './participant.component.html',
+  styleUrls: ['./participant.component.scss']
+})
+export class ParticipantComponent {
+  @Output() participant = new EventEmitter<boolean>();
+
+
+
+  currentQuestionIndex = 0;
+
+  selectedAnswer = '';
+
+  //@Output() startGameEvent = new EventEmitter<{ name: string, firstName: string, age: number }>();
+
+  participantName !: string;
+  participantFirstName !: string;
+  participantAge !: number;
+
+  constructor(private router : Router) {
+    this.participant.emit(true);
+  }
+
+  //Début de jeu
+  startGame() {
+    const participantData = {
+      name: this.participantName,
+      firstName: this.participantFirstName,
+      age: this.participantAge
+    };
+  }
+
+  ngOnInit() {
+  }
+
+  selectAnswer(answer: string) {
+    // Mettre à jour la réponse sélectionnée
+    this.selectedAnswer = answer;
+  }
+
+  submitAnswer() {
+    // Envoyer la réponse sélectionnée au serveur
+    console.log('le participant a sélectionné la réponse', this.selectedAnswer);
+
+    // Passer à la question suivante
+
+  }
+
+  goToPage(){
+    this.router.navigate([`/presentator/1`]);
+  }
+}
